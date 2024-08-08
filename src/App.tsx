483c63289646c7272cb14eb2ed1d52bc7f8d28f3
Jordan Force <jordan.force@civi.com>
2024-08-08 14:07:36 -0400

    Using generateClient from aws-amplify/api instead of aws-amplify/data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { generateClient } from "aws-amplify/api";
 const client = generateClient<Schema>();
 
 function App() {
